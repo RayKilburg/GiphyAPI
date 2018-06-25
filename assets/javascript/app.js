2708f9686b858cf1e7ac66cd93855d1dd682e8d4
RayKilburg <rkilburg42@gmail.com>
2018-06-24 23:51:16 -0400

    ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,17 @@
 var topic = [];
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF"
-
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF";
+// var klsk = $$(this).attr(data type);
 //ajax request
 $.ajax({
     url: queryURL,
     method: "GET"
   })
+
+  //.then function
+  .then(function(response){
+    var topic = response.data;
+  });
+
+  //for loop to run through data
+
+  //if else statments to run and puase gif
