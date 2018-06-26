a9a7da1656bdaa89a905c2d56cd7f0bcc83c9b4c
Ray <rkilburg42@gmail.com>
2018-06-26 14:57:47 -0400

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,12 @@
-var topic = [];
+
+
+$("button").on("click", function() {
 //var queryURL = "https://api.giphy.com/v1/gifs/search?q=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF";
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF&limit=5";
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
+person + "&api_key=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF&limit=10";
 console.log("app.js")
 // var klsk = $$(this).attr(data type);
+  var person = $(this).attr("data-person");
 //ajax request
 $.ajax({
     url: queryURL,
@@ -11,10 +15,35 @@ $.ajax({
 
   //.then function
   .then(function(response){
-    var topic = response.data; 
+    var results = response.data; 
     console.log(response)
+
+    for (var i = 0; i < results.length; i++){
+      var gifDiv = $("<div class='item'>");
+
+      var rating = results[i].rating;
+
+      var p = $("<p>").text("Rating: " + rating);
+
+      var personImage = $("<img>");
+      personImage.attr("src", results[i].images.fixed_height.url);
+
+      gifDiv.prepend(p);
+      gifDiv.prepend(personImage);
+
+      $("#gifs-appear-here").prepend(gifDiv);
+
+
+    }
   });
+
+});
 
   //for loop to run through data
 
-  //if else statments to run and puase gif
+  //if else statments to run and puase
+  //pause and stop gifs
+
+  //get user text to = result
+  
+  //user make new button
