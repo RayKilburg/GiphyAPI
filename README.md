2c37740f7b708bd42a30197a456f36ff28511b42
RayKilburg <rkilburg42@gmail.com>
2018-06-27 02:24:51 -0400

    addedsubmitbtn

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GiphyAPI
+
+Not finished yet, Still working on a couple of things. I had some trouble with this assignment.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,71 @@
+var myButtons = ["dogs", "cats"];
 
+//adding buttons to array myButtons
+function renderButtons() {
+  //stops dogs and cat buttons from repeating
+  $("#myTarget").empty();
+  for (var i = 0; i < myButtons.length; i++) {
+    $("#myTarget").append(
+      "<button class='user-button'>" + myButtons[i] + "</button>"
+    );
+    console.log(myButtons);
+  }
+}
+
+$("form").on("submit", function(event) {
+  event.preventDefault();
+  console.log($("#user-input").val());
+  myButtons.push($("#user-input").val());
+  renderButtons();
+});
+
+$(document).on("click", ".user-button", function() {
+  console.log($(this).text());
+});
 
 $("button").on("click", function() {
-//var queryURL = "https://api.giphy.com/v1/gifs/search?q=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF";
-var person = $(this).attr("data-person");
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
-person + "&api_key=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF&limit=10";
-console.log("app.js")
+  //var queryURL = "https://api.giphy.com/v1/gifs/search?q=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF";
+  var person = $(this).attr("data-person");
+  var queryURL =
+    "http://api.giphy.com/v1/gifs/search?q=" +
+    person +
+    "&api_key=sWsnA7UXhW7EQhC9uPHFvYAxl9Jo6GMF&limit=10";
+  console.log("app.js");
   // var person = $(this).attr("data-person");
-//ajax request
-$.ajax({
+  //ajax request
+  $.ajax({
     url: queryURL,
     method: "GET"
   })
 
-  //.then function
-  .then(function(response){
-    var results = response.data; 
-    console.log(response)
+    //.then function
+    .then(function(response) {
+      var results = response.data;
+      console.log(response);
 
-    for (var i = 0; i < results.length; i++){
-      var gifDiv = $("<div class='item'>");
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
 
-      var rating = results[i].rating;
+        var rating = results[i].rating;
 
-      var p = $("<p>").text("Rating: " + rating);
+        var p = $("<p>").text("Rating: " + rating);
 
-      var personImage = $("<img>");
-      personImage.attr("src", results[i].images.fixed_height.url);
+        var personImage = $("<img>");
+        personImage.attr("src", results[i].images.fixed_height.url);
 
-      gifDiv.prepend(p);
-      gifDiv.prepend(personImage);
+        gifDiv.prepend(p);
+        gifDiv.prepend(personImage);
 
-      $("#gifs-appear-here").prepend(gifDiv);
-
-
-    }
-  });
-
-
+        $("#gifs-appear-here").prepend(gifDiv);
+      }
+    });
 });
 
-  //for loop to run through data
+renderButtons();
 
-  //if else statments to run and puase
-  //pause and stop gifs
+//for loop to run through data
 
-  //get user text to = result
-  
-  //user make new button
+//if else statments to run and puase
+//pause and stop gifs
+
+//get user text to = result
